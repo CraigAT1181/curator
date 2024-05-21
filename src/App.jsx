74dff5abf1aeae8ba506a79125897681f7a90031
Craig <craigtipple81@gmail.com>
2024-05-21 14:56:02 +0100

    Implemented lazy loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,27 @@
-import React from "react";
+import React, { Suspense, lazy } from "react";
 import { Routes, Route } from "react-router-dom";
 
 import Header from "./components/Header";
-import Home from "./components/Home";
+
+const Home = lazy(() => import("./components/Home"));
 
 export default function App() {
   return (
     <div>
       <Header />
       <main>
-        <Routes>
-          <Route
-            path="/"
-            element={<Home />}
-          />
-          <Route
-            path="/home"
-            element={<Home />}
-          />
-        </Routes>
+        <Suspense fallback={<div>Loading ...</div>}>
+          <Routes>
+            <Route
+              path="/"
+              element={<Home />}
+            />
+            <Route
+              path="/home"
+              element={<Home />}
+            />
+          </Routes>
+        </Suspense>
       </main>
     </div>
   );
