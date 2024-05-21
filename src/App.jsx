f2c79aa41e1f7f2a4ec90f3bff6be9f04175fa7d
Craig <craigtipple81@gmail.com>
2024-05-21 14:43:48 +0100

    Added Header.jsx and Home.jsx. Implemented Routes and updated App.jsx to include routes for Header and Home.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,25 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
+
+import Header from "./components/Header";
+import Home from "./components/Home";
 
 export default function App() {
   return (
     <div>
-      <h1>Working!</h1>
+      <Header />
+      <main>
+        <Routes>
+          <Route
+            path="/"
+            element={<Home />}
+          />
+          <Route
+            path="/home"
+            element={<Home />}
+          />
+        </Routes>
+      </main>
     </div>
   );
 }
