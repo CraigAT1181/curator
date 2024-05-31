083d2c6ffac7b6116d9784f0b793864667d81920
Craig <craigtipple81@gmail.com>
2024-05-31 20:14:10 +0100

    Added message to YourExhibit if no items onscreen

--- src/components/YourExhibit.jsx
+++ src/components/YourExhibit.jsx
@@ -12,6 +12,12 @@ export default function YourExhibit() {
       <div className="flex justify-end">
         <button onClick={() => navigate(-1)}>Back</button>
       </div>
+      {userExhibit.length === 0 && (
+        <div className="flex text-center h-80 items-center md:justify-center">
+          <h4>Exhibits you add will appear here.</h4>
+        </div>
+      )}
+
       <div className="flex justify-center">
         <div className="grid md:grid-cols-2 lg:grid-cols-4 gap-6">
           {userExhibit.length > 0
